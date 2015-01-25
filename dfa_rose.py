f6198a4529c499e1cb491af383ed67319653ae90
rose2s <roses@Roselanes-MacBook-Pro.local>
2015-01-25 18:47:28 -0500

    identifier okay

--- dfa_rose.py
+++ dfa_rose.py
@@ -12,12 +12,18 @@ class DFA:
         return;
     
     def transition_to_state_with_input(self, input_value):
+
         inp_tuple = (self.current_state, input_value)
         print inp_tuple
-        if inp_tuple in self.transition_function.keys():
-            self.current_state = self.transition_function[(self.current_state, input_value)]
-            print "in"
-        else:
+
+        for tf2 in self.transition_function:
+            if self.current_state == tf2[0]:
+                if input_value in tf2[1]:
+                    self.current_state = self.transition_function[tf2]
+                    print self.current_state
+                print "in"
+        
+        if self.current_state != self.transition_function[tf2]:
             self.current_state = None
             print "out"
         return;
@@ -73,82 +79,23 @@ start_state = 's0'
 accept_states = {'s0','s2'}
 """
 alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y',
-            0,1,2,3,4,5,6,7,8,9,
+            '0','1','2','3','4','5','6','7','8','9',
             ":", ";", ",", "+", "-", "*", "/", "(", ")", "<", "<=", ">", ">=", "!=", "=", ":=", "{", "}"}
 
 tf = {}
 
-tf[('s0', 'a')] = 's1'
-tf[('s0', 'b')] = 's1'
-tf[('s0', 'c')] = 's1'
-tf[('s0', 'd')] = 's1'
-tf[('s0', 'e')] = 's1'
-tf[('s0', 'f')] = 's1'
-tf[('s0', 'g')] = 's1'
-tf[('s0', 'h')] = 's1'
-tf[('s0', 'i')] = 's1'
-tf[('s0', 'j')] = 's1'
-tf[('s0', 'k')] = 's1'
-tf[('s0', 'l')] = 's1'
-tf[('s0', 'm')] = 's1'
-tf[('s0', 'n')] = 's1'
-tf[('s0', 'o')] = 's1'
-tf[('s0', 'p')] = 's1'
-tf[('s0', 'q')] = 's1'
-tf[('s0', 'r')] = 's1'
-tf[('s0', 's')] = 's1'
-tf[('s0', 't')] = 's1'
-tf[('s0', 'u')] = 's1'
-tf[('s0', 'v')] = 's1'
-tf[('s0', 'x')] = 's1'
-tf[('s0', 'z')] = 's1'
-tf[('s0', 'w')] = 's1'
-tf[('s0', 'y')] = 's1'
+tf[('s0', ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y'))] = 's1'
 
-tf[('s1', 'a')] = 's1'
-tf[('s1', 'b')] = 's1'
-tf[('s1', 'c')] = 's1'
-tf[('s1', 'd')] = 's1'
-tf[('s1', 'e')] = 's1'
-tf[('s1', 'f')] = 's1'
-tf[('s1', 'g')] = 's1'
-tf[('s1', 'h')] = 's1'
-tf[('s1', 'i')] = 's1'
-tf[('s1', 'j')] = 's1'
-tf[('s1', 'k')] = 's1'
-tf[('s1', 'l')] = 's1'
-tf[('s1', 'm')] = 's1'
-tf[('s1', 'n')] = 's1'
-tf[('s1', 'o')] = 's1'
-tf[('s1', 'p')] = 's1'
-tf[('s1', 'q')] = 's1'
-tf[('s1', 'r')] = 's1'
-tf[('s1', 's')] = 's1'
-tf[('s1', 't')] = 's1'
-tf[('s1', 'u')] = 's1'
-tf[('s1', 'v')] = 's1'
-tf[('s1', 'x')] = 's1'
-tf[('s1', 'z')] = 's1'
-tf[('s1', 'w')] = 's1'
-tf[('s1', 'y')] = 's1'
+tf[('s1', ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y'))] = 's1'
 
-tf[('s1', 0)] = 's1'
-tf[('s1', 1)] = 's1'
-tf[('s1', 2)] = 's1'
-tf[('s1', 3)] = 's1'
-tf[('s1', 4)] = 's1'
-tf[('s1', 5)] = 's1'
-tf[('s1', 6)] = 's1'
-tf[('s1', 7)] = 's1'
-tf[('s1', 8)] = 's1'
-tf[('s1', 9)] = 's1'
+tf[('s1', ('0','1','2','3','4','5','6','7','8','9'))] = 's1'
 
 start_state = 's0'
 accept_states = {'s1'}
 
 d = DFA(states, alphabet, tf, start_state, accept_states)
 
-inp_program = "xda2"
+inp_program = "2sdsd"
 print inp_program
 
 print d.run_with_input_list(inp_program);
