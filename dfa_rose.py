05dbbf5fc56d08416c6e6d01100a3f6f3efccef1
rose2s <roses@Roselanes-MacBook-Pro.local>
2015-01-25 19:11:21 -0500

    starting int

--- dfa_rose.py
+++ dfa_rose.py
@@ -1,6 +1,7 @@
 class DFA:
 
     current_state = None;
+    tokenType = {"IDENTIFIER": 's1',"INTLITERAL": 's2',"FLOATLITERAL": 's3', "OPERATOR": 's4'}
 
     def __init__(self, states, alphabet, transition_function, start_state, accept_states):
         self.states = states;
@@ -9,6 +10,7 @@ class DFA:
         self.start_state = start_state;
         self.accept_states = accept_states;
         self.current_state = start_state;
+
         return;
     
     def transition_to_state_with_input(self, input_value):
@@ -46,56 +48,28 @@ class DFA:
     pass;
 
 # --- Main ---
-states = {'s0', 's1'}
+states = {'s0', 's1', 's2'}
 
-# Digit transition Function
-"""  
-tf = {}
-tf[('s0', 0)] = 's1'
-tf[('s0', 1)] = 's1'
-tf[('s0', 2)] = 's1'
-tf[('s0', 3)] = 's1'
-tf[('s0', 4)] = 's1'
-tf[('s0', 5)] = 's1'
-tf[('s0', 6)] = 's1'
-tf[('s0', 7)] = 's1'
-tf[('s0', 8)] = 's1'
-tf[('s0', 9)] = 's1'
-
-tf[('s1', '.')] = 's2'
-
-tf[('s2', 0)] = 's2'
-tf[('s2', 1)] = 's2'
-tf[('s2', 2)] = 's2'
-tf[('s2', 3)] = 's2'
-tf[('s2', 4)] = 's2'
-tf[('s2', 5)] = 's2'
-tf[('s2', 6)] = 's2'
-tf[('s2', 7)] = 's2'
-tf[('s2', 8)] = 's2'
-tf[('s2', 9)] = 's2'
-
-start_state = 's0'
-accept_states = {'s0','s2'}
-"""
 alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y',
             '0','1','2','3','4','5','6','7','8','9',
             ":", ";", ",", "+", "-", "*", "/", "(", ")", "<", "<=", ">", ">=", "!=", "=", ":=", "{", "}"}
 
 tf = {}
-
+# identifier transition
 tf[('s0', ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y'))] = 's1'
-
 tf[('s1', ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y'))] = 's1'
-
 tf[('s1', ('0','1','2','3','4','5','6','7','8','9'))] = 's1'
+# int transition
+tf[('s0', ('0','1','2','3','4','5','6','7','8','9'))] = 's2'
+tf[('s2', ('0','1','2','3','4','5','6','7','8','9'))] = 's2'
 
 start_state = 's0'
-accept_states = {'s1'}
+accept_states = {'s1','s2'}
+# id = s1, int = s2
 
 d = DFA(states, alphabet, tf, start_state, accept_states)
 
-inp_program = "2sdsd"
+inp_program = "111"
 print inp_program
 
 print d.run_with_input_list(inp_program);
