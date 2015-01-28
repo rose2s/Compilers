6faf1b07e6dda4fbd2895bb1a145307a4856913d
rose2s <roses@Roselanes-MacBook-Pro.local>
2015-01-28 16:23:48 -0500

    longer program

--- test_program.py
+++ test_program.py
@@ -1,8 +1,13 @@
 procedure sum():
-// comment
-// int a ;
-// a = var + int ;
-//	if a != 5:
-//		print 1
-//	case :=
-//		a = 2 - 2;
+//comment
+int a;
+a = var + int ;
+if a != 5:
+	print 1 
+else
+	print a
+case 1 = 2
+	a = 2+2;
+else
+b = 2 - 2
+if 2.0. + int * 2!=
