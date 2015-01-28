d5c7265d10e42d0654a2036c637ace9b65138e2e
rose2s <roses@Roselanes-MacBook-Pro.local>
2015-01-28 15:17:42 -0500

    Inicializes Automata and create aritm_op

--- automata.py
+++ automata.py
@@ -2,20 +2,46 @@ class DFA:
 
     current_state = None;
 
-    def __init__(self, states, alphabet, transition_function, start_state, accept_states):
-        self.states = states;
-        self.alphabet = alphabet;
-        self.transition_function = transition_function;
-        self.start_state = start_state;
-        self.accept_states = accept_states;
-        self.current_state = start_state;
+    def __init__(self):
+
+        self.states = {'s0', 's1', 's2','s3','s4','s5','s6','s7','s8'}
+        self.alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y',
+                    '0','1','2','3','4','5','6','7','8','9',
+                    ":", ";", ",", "+", "-", "*", "/", "(", ")", "<", ">", "!", "=", "{", "}"}
+
+        self.start_state = 's0'
+
+        # id = s1, int = s2, float=s4, comp=s6,s7, aritm_op=s8, s9 = equal
+        self.accept_states = {'s1','s2','s3','s6', 's7','s8','s9','s10'}
+
+        tf = {}
+        # identifier transition
+        tf[('s0', ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y'))] = 's1'
+        tf[('s1', ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y'))] = 's1'
+        tf[('s1', ('0','1','2','3','4','5','6','7','8','9'))] = 's1'
+        # int transition
+        tf[('s0', ('0','1','2','3','4','5','6','7','8','9'))] = 's2'
+        tf[('s2', ('0','1','2','3','4','5','6','7','8','9'))] = 's2'
+        # float transition 
+        tf[('s0',('.'))] = 's4'
+        tf[('s4', ('0','1','2','3','4','5','6','7','8','9'))] = 's3'
+        tf[('s2',('.'))] = 's3'
+        tf[('s3', ('0','1','2','3','4','5','6','7','8','9'))] = 's3'
+        # operator token
+        tf[('s0',("!",':'))] = 's5'
+        tf[('s0',("<", ">"))] = 's6'
+        tf[('s5',("="))] = 's7'
+        tf[('s6',("="))] = 's7'
+        tf[('s0',("+", "-", "*", "/"))] = 's8'
+        tf[('s0',("="))] = 's9' 
+        # separator token
+        tf[('s0',("(", ")", "{", "}",";",","))] = 's10'
+
+        self.transition_function = tf
 
         return;
     
     def transition_to_state_with_input(self, input_value):
-
-        inp_tuple = (self.current_state, input_value)
-        #print inp_tuple
 
         for tf2 in self.transition_function:  #d[tf2,(tf1)] = sx
             if self.current_state == tf2[0]:   
@@ -44,6 +70,8 @@ class DFA:
     # processes each character individualy
     def run_with_input_list(self, input_list):
         self.go_to_initial_state()
+        print input_list
+        
         for inp in input_list:
             self.transition_to_state_with_input(inp)
             continue
